b5b18d074d4639dc1693c4fc6879abb617b0a3ea
monivothkol <66184217+monivothkol@users.noreply.github.com>
2024-02-17 17:01:11 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-        user: 'postgres',
+        //user: 'postgres',
         host: 'postgres://mebdubdr:BGGguNuWZylRx0NOywm8JqeTf0DKmcvb@bubble.db.elephantsql.com/mebdubdr',
         // database: 'smartnas_db',
-        password: 'BGGguNuWZylRx0NOywm8JqeTf0DKmcvb',
-        port: '5432',
-        ssl: {
-                rejectUnauthorized: false
-            }
+        //password: 'BGGguNuWZylRx0NOywm8JqeTf0DKmcvb',
+        //port: '5432',
+        //ssl: {
+            //    rejectUnauthorized: false
+           // }
 });
 
 
