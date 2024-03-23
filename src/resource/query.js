b03877655640891e1644aa0c3afbb0bfb76fe5e9
MacBook <monivothkol1999@gmail.com>
2024-03-23 13:05:29 +0700

    update main balance

--- src/resource/query.js
+++ src/resource/query.js
@@ -2,7 +2,7 @@
 const getUserResource = "select * from user_resource where phonenumber = $1";
 const updateMainBalance = `
     UPDATE user_resource 
-    SET mainbalance = $1 
+    SET mainbalance = mainbalance + $1 
     WHERE phonenumber = $2
 `;
 module.exports = {
